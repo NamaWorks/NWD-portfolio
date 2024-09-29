eb2ba146c4f30ed44d1a4b2352c313fa1e1b3d48
NamaWorks <pedronayamartinez@hotmail.com>
2024-09-29 21:43:22 +0200

    add all items in the project entries

--- src/Components/ProjectEntry/ProjectEntry.jsx
+++ src/Components/ProjectEntry/ProjectEntry.jsx
@@ -35,24 +35,25 @@ const ProjectEntry = ({ prObject }) => {
           <h3 className="pr-name">{prObject.title} // <span className="dimmed">{prObject.kind}</span></h3>
           <h3 className="pr-year dimmed">{prObject.year}</h3>
           {
-            prObject.links.code.frontEnd && (
-                <a href={prObject.links.code.frontEnd} target="_blank" className="pr-code-frontEnd dimmed">[gh-frontend]</a>
-            )
+            prObject.links.code.frontEnd ?
+                (<a href={prObject.links.code.frontEnd} target="_blank" className="pr-code-frontEnd dimmed">[gh-frontend]</a>)
+                : (<a href={prObject.links.code.frontEnd} target="_blank" className="pr-code-frontEnd dimmed">[gh-frontend]</a>)
+            
           }
           {
-            prObject.links.code.backEnd && (
-                <a href={prObject.links.code.backEnd} target="_blank" className="pr-code-backEnd dimmed">[gh-backend]</a>
-            )
+            prObject.links.code.backEnd ? 
+              (<a href={prObject.links.code.backEnd} target="_blank" className="pr-code-backEnd dimmed">[gh-backend]</a>)
+              : (<a href={prObject.links.code.backEnd} target="_blank" className="pr-code-backEnd dimmed inactive">[gh-backend]</a>)
           }
           {
-            prObject.links.deployment.frontEnd && (
-                <a href={prObject.links.deployment.frontEnd} target="_blank" className="pr-deployment-frontEnd dimmed">[deployment-frontend]</a>
-            )
+            prObject.links.deployment.frontEnd ? 
+            (<a href={prObject.links.deployment.frontEnd} target="_blank" className="pr-deployment-frontEnd dimmed">[deployment-frontend]</a>)
+            : (<a href={prObject.links.deployment.frontEnd} target="_blank" className="pr-deployment-frontEnd dimmed inactive">[deployment-frontend]</a>)
           }
           {
-            prObject.links.deployment.backEnd && (
-                <a href={prObject.links.deployment.backEnd} target="_blank" className="pr-deployment-backEnd dimmed">[deployment-backend]</a>
-            )
+            prObject.links.deployment.backEnd ? 
+                (<a href={prObject.links.deployment.backEnd} target="_blank" className="pr-deployment-backEnd dimmed">[deployment-backend]</a>)
+                : (<a href={prObject.links.deployment.backEnd} target="_blank" className="pr-deployment-backEnd dimmed inactive">[deployment-backend]</a>)
           }
         </div>
 
